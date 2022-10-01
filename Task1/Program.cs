270f6f33edce25d17c7231be125f866bdc6ef5fb
Anastasia <silutina.n@yandex.ru>
2022-10-01 15:13:00 +0300

    create task1

---
+++ Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("проверка связи!");
+// По двум заданным числам проверять является ли первое квадратом второго
+int A = 17;
+int B = 4;
+if (B*B==A)
+Console.WriteLine("Является");
+else Console.WriteLine("Не является");
