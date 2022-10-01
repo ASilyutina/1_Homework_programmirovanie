427e772049695b0052aa7474121d51ed6b93876e
Anastasia <silutina.n@yandex.ru>
2022-10-01 22:14:13 +0300

    save task5

---
+++ Task5/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите длину: ");
+int n = Convert.ToInt32(Console.ReadLine());
+    
+for (int i = 2; i <= n; i += 2)
+    Console.WriteLine(i);
