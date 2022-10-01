ea5e8e8924894d87923ea8c795612d89ea666a89
Anastasia <silutina.n@yandex.ru>
2022-10-01 18:58:00 +0300

    save task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти максимальное из трех чисел
+int A = 70;
+int B = 100;
+int C = 15;
+ if (A > B && A > C)
+                    Console.WriteLine("A"); 
+                else if (B > C)
+                    Console.WriteLine("B"); 
+                else
+                    Console.WriteLine("C"); 
+
+
+
