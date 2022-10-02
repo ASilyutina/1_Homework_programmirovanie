816d2f5d57a4b0e3c891195c510a891229a5d542
Anastasia <silutina.n@yandex.ru>
2022-10-02 14:17:45 +0300

    save changes in task4-new

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,5 +1,17 @@
 ﻿// Выяснить является ли число чётным
  Console.WriteLine("введите число: ");
  int n = Convert.ToInt32(Console.ReadLine()); 
-if (n % 2 == 0) Console.WriteLine("четное число");
-else Console.WriteLine("нечетное");
+if (n % 2 == 0) Console.WriteLine($"число {n} четное число");
+else Console.WriteLine($"число {n} не четное число");
+
+// 2ой способ Выяснить является ли число чётным
+
+Console.Write("Введите число:    ");
+string writeNumber = Console.ReadLine();
+int number = Convert.ToInt32(writeNumber);
+string result = $"Число {number} является чётным";
+
+if (number % 2 != 0) result = result.Replace("является", "не является");
+
+Console.WriteLine(result);
+Console.WriteLine();
