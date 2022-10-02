990d29aa51a0bcecb9cc0b0ce2e34c114d89daf1
Anastasia <silutina.n@yandex.ru>
2022-10-02 14:23:06 +0300

    save changes in task5-new

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -5,3 +5,17 @@ int n = Convert.ToInt32(Console.ReadLine());
     
 for (int i = 2; i <= n; i += 2)
     Console.WriteLine(i);
+
+// 2ой способ -Показать четные числа от 1 до N
+
+Console.Write("Введите число:   ");
+string txtN = Console.ReadLine();
+int endNum = Convert.ToInt32(txtN);
+int num = 0;
+Console.WriteLine($"Чётные числа из диапазона от 1 до {endNum}:");
+while (num < endNum)
+{
+    num++;
+    if (num % 2 == 0) Console.Write($"{num} ");
+}
+Console.WriteLine(); Console.WriteLine();
