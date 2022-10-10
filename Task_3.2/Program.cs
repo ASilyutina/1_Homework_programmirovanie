751dd579290ba1cb47a492972a80fde525a8c893
Anastasia <silutina.n@yandex.ru>
2022-10-10 13:21:23 +0300

    save task3.2+

--- Task_3.2/Program.cs
+++ Task_3.2/Program.cs
@@ -1,20 +1,34 @@
 ﻿// Подсчитать сумму цифр в числе
 
-System.Console.Write("Введите число:   ");
-string writeA = Console.ReadLine();
-int A = Convert.ToInt32(writeA);
+//System.Console.Write("Введите число:   ");
+//string writeA = Console.ReadLine();
+//int A = Convert.ToInt32(writeA);
 
-int GetNumbersSum(int A)
+//int GetNumbersSum(int A)
+//{
+//    int number = 1;
+//    int sum = 0;
+  //  for (int i = 0; i < A; i++)
+    //{
+    //    sum = sum + number;
+      //  number++;
+   // }
+  //  return sum;
+//}
+
+//int NumbersSum = GetNumbersSum(A);
+//System.Console.WriteLine($"Сумма чисел от 1 до {A} составляет {NumbersSum}");
+
+int number = new Random().Next(1, 100000);
+int sumDigit = 0;
+System.Console.Write($"Сумма цифр числа {number} равна ");
+
+while (number > 0)
 {
-    int number = 1;
-    int sum = 0;
-    for (int i = 0; i < A; i++)
-    {
-        sum = sum + number;
-        number++;
-    }
-    return sum;
+    int digit = number % 10;
+    sumDigit = sumDigit + digit;
+    number = number / 10;
 }
 
-int NumbersSum = GetNumbersSum(A);
-System.Console.WriteLine($"Сумма чисел от 1 до {A} составляет {NumbersSum}");
+System.Console.WriteLine(sumDigit);
+System.Console.WriteLine();
